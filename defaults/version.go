7b4ed61324c5518ec2b58d73ce2fa756051c589a
MickMake <github@mickmake.com>
2020-05-27 21:09:20 +1000

    Commit before release v1.0.2

--- defaults/version.go
+++ defaults/version.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName = "buildtool"
-	BinaryVersion = "1.0.1"
+	BinaryVersion = "1.0.2"
 	BinaryRepo = "gearboxworks/buildtool"
 	SourceRepo = "newclarity/buildtool"
 )
